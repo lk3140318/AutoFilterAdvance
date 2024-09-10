672047d7f50aa6b66be3c17bdf3d9d845efac0d2
Luck Mix <153753933+lk3140318@users.noreply.github.com>
2024-09-11 00:31:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/AutoFilterAdvance /AutoFilterAdvance
+  git clone https://github.com/lk3140318/AutoFilterAdvance/blob/TECH_VJ/start.sh /AutoFilterAdvance
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AutoFilterAdvance
